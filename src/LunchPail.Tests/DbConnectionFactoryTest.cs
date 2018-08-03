8110965e6ea5d919f69cbff27c1b01b36a8c19f3
Pim Brouwers <pbrouwers@nhlpa.com>
2018-08-03 07:44:54 -0400

    readme, code cleanup

--- src/LunchPail.Tests/DbConnectionFactoryTest.cs
+++ src/LunchPail.Tests/DbConnectionFactoryTest.cs
@@ -1,10 +1,6 @@
 ﻿using Moq;
 using System;
-using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace LunchPail.Tests
--- src/LunchPail.Tests/UnitOfWorkTest.cs
+++ src/LunchPail.Tests/UnitOfWorkTest.cs
@@ -1,8 +1,6 @@
 ﻿using Moq;
 using System;
-using System.Collections.Generic;
 using System.Data;
-using System.Text;
 using Xunit;
 
 namespace LunchPail.Tests
